e913e1cbdf7b59237cf860a81210d82f7e8dd50c
Ravil Berishev <quadraticteam@gmail.com>
2022-05-31 21:01:19 +0300

    KarmaPrivateSaleVestingVault

--- contracts/privateSale/KarmaPrivateSaleVestingVault.sol
+++ contracts/privateSale/KarmaPrivateSaleVestingVault.sol
@@ -2,8 +2,10 @@
 pragma solidity ^0.8.0;
 
 import "../utils/vesting/VestingVaultAccessControl.sol";
+import "../utils/vesting/VestingVaultBase.sol";
+import "../utils/vesting/IVestingVault.sol";
 
-contract KarmaPrivateSaleVestingVault is VestingVaultAccessControl {
+contract KarmaPrivateSaleVestingVault is VestingVaultAccessControl, IVestingVault {
     constructor(
         address karmaToken,
         uint256[] memory vestingPortionsUnlockTime,
@@ -13,6 +15,10 @@ contract KarmaPrivateSaleVestingVault is VestingVaultAccessControl {
         _setToken(karmaToken);
         _setZeroDate(block.timestamp);
         _setVestingInfo(vestingPortionsUnlockTime, vestingPercentPerPortion, vestingPercentPrecision);
+    }
+
+    function getBeneficiary(address beneficiary) public view override(VestingVaultBase, IVestingVault) returns (uint256 amount, bool[] memory isPortionWithdraw) {
+        return super.getBeneficiary(beneficiary);
     }
 
     function setZeroDate(uint256 zeroDate) external {
