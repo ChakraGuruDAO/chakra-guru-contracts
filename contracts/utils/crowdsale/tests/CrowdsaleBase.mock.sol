6b2f09430e73dc29aa09cdca0568cd5074dc61e1
Ravil Berishev <quadraticteam@gmail.com>
2022-05-31 21:54:17 +0300

    Crowdsale empty mocks

---
+++ contracts/utils/crowdsale/tests/CrowdsaleBase.mock.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../CrowdsaleBase.sol";
+
+contract CrowdsaleBaseMock is CrowdsaleBase {}
---
+++ contracts/utils/crowdsale/tests/validation/CrowdsaleTime.mock.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../../validation/CrowdsaleTime.sol";
+
+contract CrowdsaleTimeMock is CrowdsaleTime {}
