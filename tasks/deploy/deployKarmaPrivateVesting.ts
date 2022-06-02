df21fbeab07c6409f95a321fa396d4a519c45d97
Ravil Berishev <quadraticteam@gmail.com>
2022-06-02 11:25:54 +0300

    set vesting rules

--- tasks/deploy/deployKarmaPrivateVesting.ts
+++ tasks/deploy/deployKarmaPrivateVesting.ts
@@ -2,15 +2,26 @@ import { BigNumber, BigNumberish } from "ethers";
 import { task, types } from "hardhat/config";
 import { Duration } from "~/test/_base";
 
-// export const vestingPortionsUnlockTime: BigNumberish[] = [
-//   0, 2592000, 5184000, 7776000, 10368000, 12960000, 15552000, 18144000, 20736000, 23328000, 25920000, 28512000, 31104000, 33696000, 36288000, 38880000,
-// ].map((m) => BigNumber.from(m));
-// export const vestingPercentPerPortion: BigNumberish[] = [10000, 0, 0, 0, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500].map((m) => BigNumber.from(m));
-// export const vestingPercentPrecision: BigNumberish = BigNumber.from(100000);
-
-export const vestingPortionsUnlockTime: BigNumberish[] = [0, Duration().minutes(5).toNumber(), Duration().minutes(10).toNumber()].map((m) => BigNumber.from(m));
-export const vestingPercentPerPortion: BigNumberish[] = [30, 35, 35].map((m) => BigNumber.from(m));
-export const vestingPercentPrecision: BigNumberish = BigNumber.from(100);
+export const vestingPortionsUnlockTime: BigNumberish[] = [
+  0,
+  Duration().days(30).toNumber(),
+  Duration().days(60).toNumber(),
+  Duration().days(90).toNumber(),
+  Duration().days(120).toNumber(),
+  Duration().days(150).toNumber(),
+  Duration().days(180).toNumber(),
+  Duration().days(210).toNumber(),
+  Duration().days(240).toNumber(),
+  Duration().days(270).toNumber(),
+  Duration().days(300).toNumber(),
+  Duration().days(330).toNumber(),
+  Duration().days(360).toNumber(),
+  Duration().days(390).toNumber(),
+  Duration().days(420).toNumber(),
+  Duration().days(450).toNumber(),
+].map((m) => BigNumber.from(m));
+export const vestingPercentPerPortion: BigNumberish[] = [10000, 0, 0, 0, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500, 7500].map((m) => BigNumber.from(m));
+export const vestingPercentPrecision: BigNumberish = BigNumber.from(100000);
 
 task("deploy:karma_private_sale_vesting_vault", "Deploy KARMA Private sale Vesting vault")
   .addParam("karmatoken", "KARMA Token address", null, types.string)
